01dfa6ae2e76921db67a701f398519493edb44ac
minupal <minupal6@gmail.com>
2017-01-25 15:32:07 -0800

    postbuild script

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -8,7 +8,6 @@ var config = require('./shared.js')
 
 module.exports = merge(config, {
   output: { filename: "[name]-[hash].js" },
-
   plugins: [
     new webpack.LoaderOptionsPlugin({
       minimize: true
