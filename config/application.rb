a5a26a5a41a555e71b4881bef4e1f1b2b03a3d83
minupal <minupal6@gmail.com>
2017-01-26 13:08:27 -0800

    app rb add webfonts

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
-
 require 'rails/all'
+include Google::Webfonts::Helper
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
